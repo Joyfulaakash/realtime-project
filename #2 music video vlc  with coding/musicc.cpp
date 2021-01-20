5a06b319e1404c3c0495218a160f516c89a5b80f
Aakash J <53146819+codejoyful@users.noreply.github.com>
2021-01-20 19:50:37 +0530

    Add files via upload

---
+++ #2 music video vlc  with coding/musicc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+   
+    system("vlc song.mp3");
+    system("vlc video.mp4"); //access the terminal linux
+    
+    
+    
+    //system("%PROGRAMFILES%\\VideoLAN\\VLC\\vlc.exe song.mp3") //access cmd or powershell windows
+    return 0;
+}
+
---
+++ #2 music video vlc  with coding/musicp.py
@@ -0,0 +1,10 @@
+import os
+
+file = "song.mp3"
+file1 = "video.mp4"
+os.system("vlc " + file)
+os.system("vlc " + file1)
+
+
+
+
